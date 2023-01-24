f09477d2b8efd37eed66568e61ae804f7b971e37
Ltwoz <warrapat.102@gmail.com>
2023-01-24 16:26:37 +0700

    test

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         domains: ["media.discordapp.net", "cdn.discordapp.com"],
     },
     env: {
-        MONGODB_URI: "mongodb+srv://vercel-admin-user-63cf571b8d375c46538895bc:h81nm6s1ygA5MPIA@skitzer-shop.r68opm8.mongodb.net/myFirstDatabase?retryWrites=true",
+        MONGODB_URI: "mongodb://127.0.0.1/svrt-test",
         JWT_SECRET: "SRVTRENTWEBSECRETJWT",
         JWT_EXPIRE: "5d"
     },
