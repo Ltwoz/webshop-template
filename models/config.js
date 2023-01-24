7f06ec19696bf0d8afab87f1348358c040ba1615
Ltwoz <warrapat.102@gmail.com>
2023-01-24 11:23:57 +0700

    config model

--- models/config.js
+++ models/config.js
@@ -4,42 +4,30 @@ const ConfigSchema = mongoose.Schema(
     {
         website_title: {
             type: String,
-            required: [true, "Please provide a Website Title."],
-            default: "SVRT E-Commerce Provider For Rent.",
         },
         website_name: {
             type: String,
-            required: [true, "Please provide a Website Title."],
-            default: "SVRT",
         },
         website_desc: {
             type: String,
-            required: [true, "Please provide a Website Description."],
-            default: "SVRT E-Commerce Provider For Rent.",
         },
         website_icon: {
             type: String,
-            required: [true, "Please provide a Website Icon."],
         },
         website_logo: {
             type: String,
-            required: [true, "Please provide a Website Logo."],
         },
         website_banner: {
             type: String,
-            required: [true, "Please provide a Website Banner."],
         },
         announcement: {
             type: String,
-            required: true,
         },
         recaptcha_key: {
             type: String,
-            required: [true, "Please provide a Recaptcha Key."],
         },
         payment_tw_phone: {
             type: String,
-            required: [true, "Please provide a Truemoney Phone Number."],
             maxLength: [10, "Phone number cannot be more than 10 characters"],
         },
         style: {
